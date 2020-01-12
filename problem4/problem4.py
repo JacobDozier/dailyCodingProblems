3aa64544f5ffc2738779a098a0824b9d7dee857f
Jacob Dozier <jacobadozier@gmail.com>
2020-01-12 00:57:00 -0600

    Found a bug for checking if 1 is the missing number.

--- problem4/problem4.py
+++ problem4/problem4.py
@@ -1,32 +1,41 @@
-arr = [-1, 0, 3, -4, 4, 2]
+arr = [-1, 0, -4, 1, 4, 2]
 missingNo = 0
 
 arr.sort()
 
-def clearNegatives(arr):
+def separateNegatives(arr):
     newArr = []
+    negatvieArr = []
     for number in arr:
         if number >= 0:
             newArr.append(number)
-    return newArr
+        else:
+            negatvieArr.append(number)
+    return newArr, negatvieArr
             
 
-arr = clearNegatives(arr)
-lowestValue = arr[-1]
+arr, negArr = separateNegatives(arr)
 
+# TODO Is the lowest value necessary?
+lowestValue = arr[-1]
 for number in arr:
     if number > 0 and number < lowestValue:
         lowestValue = number
 
+print(lowestValue)
+
 prevNum = arr[0]
 for value in arr:
-    if arr.index(value) != 0:
-        if (value - prevNum) > 1 and prevNum == lowestValue:
-            missingNo = (lowestValue + 1)
-            break
-        else:
-            missingNo = value + 1
+    if (value - prevNum) > 1 and prevNum == lowestValue:
+        missingNo = (lowestValue + 1)
+        break
+    # TODO This will always satisfy if 1 is in the list and exit the loop.
+    elif (value - prevNum) == 1 and prevNum == lowestValue:
+        missingNo = 1
+        break
+    else:
+        missingNo = value + 1
     prevNum = value
 
-print(arr)
+print(str(negArr + arr))
 print(missingNo)
