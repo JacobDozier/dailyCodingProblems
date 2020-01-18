1105b5cfa9421ca4c7e6da5dc0abb992ecf725ed
Jacob Dozier <jacobadozier@gmail.com>
2020-01-17 21:35:55 -0600

    Implemented car() and cdr(), the solution.

--- problem5/problem5.py
+++ problem5/problem5.py
@@ -3,5 +3,19 @@ def cons(a, b):
         return f(a, b)
     return pair
 
+def getPair(a, b):
+    fTuple = (a, b)
+    return fTuple
+
+def car(f):
+    firstValue = f(getPair)
+    return firstValue[0]
+
+def cdr(f):
+    lastValue = f(getPair)
+    return lastValue[-1]
+
 if __name__ == "__main__":
-# TODO
+    print("Pair: (3, 4)")
+    print("car: " + str(car(cons(3, 4))))
+    print("cdr: " + str(cdr(cons(3, 4))))
