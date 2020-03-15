6645b4a02d7fd153a87e8642418ba4c51d964bc5
Jacob Dozier <jacobadozier@gmail.com>
2020-03-14 19:09:48 -0500

    No loop needed. Refactor to remove loop.

--- problem474/problem474.py
+++ problem474/problem474.py
@@ -8,36 +8,30 @@ nickels = 0
 pennies = 0
 counter = 0
 
-# Need to start with the larges coin (25c) and work my way down.
-# Divide cents by coin size to determine how many should be in there.
-# Take the remainder and use that for the next size down.
-# In this example it would be 0 quarters 1 dime 1 nickel 1 penny. numCoins = 3.
-
 print("cents: " + str(cents))
 
-while(productOfCoins != originalCents):
-    if(cents > 25):
-        quarters = cents // 25
-        cents -= quarters * 25
-        numCoins += quarters
-        productOfCoins += quarters * 25
-        print("quarters: " + str(quarters))
-    elif(cents > 10):
-        dimes = cents // 10
-        cents -= dimes * 10
-        numCoins += dimes
-        productOfCoins += dimes * 10
-        print("dimes: " + str(dimes))
-    elif(cents > 5):
-        nickels = cents // 5
-        cents -= nickels * 5
-        numCoins += nickels
-        productOfCoins += nickels * 5
-        print("nickels: " + str(nickels))
-    else:
-        pennies = cents
-        numCoins += pennies
-        productOfCoins += pennies
-        print("pennies: " + str(pennies))
+if(cents > 25):
+    quarters = cents // 25
+    cents -= quarters * 25
+    numCoins += quarters
+    productOfCoins += quarters * 25
+    print("quarters: " + str(quarters))
+if(cents > 10):
+    dimes = cents // 10
+    cents -= dimes * 10
+    numCoins += dimes
+    productOfCoins += dimes * 10
+    print("dimes: " + str(dimes))
+if(cents > 5):
+    nickels = cents // 5
+    cents -= nickels * 5
+    numCoins += nickels
+    productOfCoins += nickels * 5
+    print("nickels: " + str(nickels))
+
+pennies = cents
+numCoins += pennies
+productOfCoins += pennies
+print("pennies: " + str(pennies))
 
 print("numCoins: " + str(numCoins))
