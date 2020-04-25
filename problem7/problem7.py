140328f3b9ed8c660bb810c1585f74a8e94dd0a1
Jacob Dozier <jacobadozier@gmail.com>
2020-04-25 03:05:11 -0500

    Adding function to find which letters are in the users input.

--- problem7/problem7.py
+++ problem7/problem7.py
@@ -6,12 +6,27 @@ def getAlphaMap():
         dictionary.update(dict.fromkeys(string.ascii_lowercase[position], position))
     
     for key in dictionary.keys():
-        dictionary[key] = dictionary.get(key) + 1
+        dictionary[key] = str(dictionary.get(key) + 1)
 
     return dictionary
 
+def findCharsInInput(passedAlphaMap, passedInput):
+    foundChars = {}
+    for key, value in passedAlphaMap.items():
+        if value in passedInput:
+            foundChars.update(((key, value),))
+    return foundChars
+
+
 if __name__ == "__main__":
+    userInput = '1213'
+    userInputCopy = userInput
+    numSolutions = 0
     alphaMap = getAlphaMap()
-    # TODO implement
-    print("Work in progress")
-    print(alphaMap)
+
+    foundChars = findCharsInInput(alphaMap, userInput)
+    print('sorted foundChars:', foundChars)
+    print('foundChars length:', str(len(foundChars)))
+
+    print('numSolutions:', numSolutions)
+    print('done')
